2ac31edf56b9d8170fd6179e1f88d7719fcd48cb
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-05-14 12:51:33 +0600

    create navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,26 @@
+import { NavLink } from 'react-router-dom';
+import '../styles/component/navigation.css';
+
+const activeLink = (isActive: boolean) => (isActive ? 'active' : '');
+
 export default function Navigation() {
-  return <h1>Nav</h1>;
+  return (
+    <nav>
+      <NavLink className={({ isActive }) => activeLink(isActive)} to="alarm">
+        Alarm
+      </NavLink>
+      <NavLink className={({ isActive }) => activeLink(isActive)} to="/">
+        Clock
+      </NavLink>
+      <NavLink className={({ isActive }) => activeLink(isActive)} to="timer">
+        Timer
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => activeLink(isActive)}
+        to="stopwatch"
+      >
+        Stopwatch
+      </NavLink>
+    </nav>
+  );
 }
