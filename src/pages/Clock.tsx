2e4a3905984c8292f379a5603a79986734d364d7
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-05-14 14:12:56 +0600

    display  time and date

--- src/pages/Clock.tsx
+++ src/pages/Clock.tsx
@@ -1,3 +1,48 @@
+import { useEffect, useState } from 'react';
+import '../styles/pages/clock.css';
+
+// TODO: Make this a state so that user can customize
+const clockOptions: Intl.DateTimeFormatOptions = {
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  weekday: 'short',
+  month: 'long',
+  day: 'numeric',
+};
+
 export default function Clock() {
-  return <h1>Clock</h1>;
+  const [parts, setParts] = useState(
+    new Intl.DateTimeFormat('default', clockOptions).formatToParts(new Date())
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setParts(
+        new Intl.DateTimeFormat('default', clockOptions).formatToParts(
+          new Date()
+        )
+      );
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className="clock">
+      <div className="local">
+        <span>
+          <h1>
+            {`${parts[6].value}:${parts[8].value}:${parts[10].value}`}{' '}
+            <span>{`${parts[12].value}`}</span>
+          </h1>
+          <p>{`${parts[0].value}, ${parts[2].value} ${parts[4].value}`}</p>
+        </span>
+      </div>
+      {/* if custom */}
+      <button className="add-custom">+</button>
+    </div>
+  );
 }
